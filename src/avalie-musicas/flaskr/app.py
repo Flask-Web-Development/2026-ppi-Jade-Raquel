568e06f21e3888aadfb405de83b0b359a1ccfc4d
Jadeoriginal <161791934+Kelzin@users.noreply.github.com>
2026-08-16 15:19:00 -0300

    feat: add editar_musica.html and basic CRUD

--- src/avalie-musicas/flaskr/app.py
+++ src/avalie-musicas/flaskr/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 db = SQLAlchemy()
 db.init_app(app)
 
-from models import Musica
+from .models import Musica
 
 @app.route("/", methods=["GET", "POST"])
 def adicionar_musica():
@@ -29,10 +29,45 @@ def adicionar_musica():
         db.session.add(musica)
         db.session.commit()
 
-        return "Música adicionado com sucesso!"
-
-    with app.app_context():
-        db.create_all()
-        
+        return redirect(url_for("musicas"))
     return render_template("index.html")
+
+@app.route("/musicas")
+def musicas():
+    musicas = db.session.execute( db.select(Musica)).scalars().all()
+    return render_template( "musicas.html", musicas=musicas)
+
+@app.route("/musicas/<int:id>/editar", methods=["GET", "POST"])
+def editar_musica(id):
+    musica = db.get_or_404(Musica, id)
+    if request.method == "POST":
+
+        musica.titulo = request.form["titulo"]
+        musica.autor = request.form["autor"]
+        musica.nota = float(request.form["nota"])
+        musica.avaliacao = request.form["avaliacao"]
+
+        db.session.commit()
+
+        return redirect(url_for("musicas"))
+
+    return render_template(
+        "editar_musica.html",
+        musica=musica
+    )
+
+@app.route("/musicas/<int:id>/deletar", methods=["POST"])
+def deletar_musica(id):
+
+    print("MÉTODO RECEBIDO:", request.method)
+
+    musica = db.get_or_404(Musica, id)
+
+    db.session.delete(musica)
+    db.session.commit()
+
+    return redirect(url_for("musicas"))
+
+with app.app_context():
+    db.create_all()
     
